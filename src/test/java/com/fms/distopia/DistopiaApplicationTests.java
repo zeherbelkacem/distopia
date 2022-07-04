07f259568749166fef952a19935c474993c2cc1c
Stagiaires11 <Stagiaires11P@ad.fms-ea.com>
2022-07-04 22:21:37 +0200

    add README.md and first commit

---
+++ src/test/java/com/fms/distopia/DistopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fms.distopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
